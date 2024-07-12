d27393772d8abf89c654b0b6059efd7eaef9aef0
chirag640 <chaudharychirag640@gmail.com>
2024-07-12 10:22:57 +0530

    VowelOrConsonant code is added

---
+++ VowelOrConsonant.java
@@ -0,0 +1,11 @@
+public class VowelOrConsonant {
+    public static void main(String[] args) {
+        char c = 'a';
+
+        if(c == 'a' || c == 'e' || c=='i' || c=='o' || c=='u'){
+            System.out.println(c + " is vowel");
+        }else{
+            System.out.println(c+" is Consonant");
+        }
+    }
+}
