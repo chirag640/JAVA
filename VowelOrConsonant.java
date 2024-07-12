845c229e37e4605996f6a0a1dadcd633927df2c8
chirag640 <chaudharychirag640@gmail.com>
2024-07-12 10:31:01 +0530

    added more code in VowelOrConsonant.java

--- VowelOrConsonant.java
+++ VowelOrConsonant.java
@@ -1,11 +1,33 @@
 public class VowelOrConsonant {
     public static void main(String[] args) {
-        char c = 'a';
+        char character = 'a';
 
-        if(c == 'a' || c == 'e' || c=='i' || c=='o' || c=='u'){
-            System.out.println(c + " is vowel");
+        simpleWay(character);
+        usingArray(character);
+    }
+    public static void simpleWay(char character){
+        if(character == 'a' || character == 'e' || character=='i' || character=='o' || character=='u'){
+            System.out.println(character + " is vowel");
         }else{
-            System.out.println(c+" is Consonant");
+            System.out.println(character+" is Consonant");
+        }
+    }
+    public static void usingArray(char character){
+        char[] vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+        boolean isVowel=false;
+        for (char d : vowels) {
+            if(character == d){
+                isVowel = true;
+                break;
+            }
+        }
+        if (isVowel) {
+            System.out.println(character + " is a vowel");
+        } else if (Character.isLetter(character)) {
+            System.out.println(character + " is a consonant");
+        } else {
+            System.out.println(character + " is not a valid alphabet letter");
         }
     }
 }
